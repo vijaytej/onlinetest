0905d5f3780fbdfb55d5d0007760d0c6636e0d1e
vijju786 <devopsvijaytej@gmail.com>
2018-06-11 13:19:26 +0530

    ping

---
+++ pingpong.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "enter n value"
+read n
+for ( i=1; i<=n; i++)
+do
+	if [ $i%3 -eq 0 ]
+	then
+		echo "ping"
+	elif [ $i%5 -eq 0 ]
+	then
+		echo "pong"
+	elif [ $i%3 -eq o && $i%5 -eq 0 ]
+		echo "ping pong"
+	else
+		echo $i
+	fi
+done
+
